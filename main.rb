7ba16fc51541a07baccec3af0a7cc7bdaf1d53db
Jyoti <jyotikiran12@gmail.com>
2020-09-15 13:22:02 +0530

    Add my_each_with_index method.

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module Enumerable
+
   # my_each method
 
   def my_each
@@ -10,3 +13,13 @@ module Enumerable
   end
 
   #----------#my_each_with_index-----------
+
+  def my_each_with_index
+    index = 0
+    while index < length
+      yield(self[index], index)
+      index += 1
+    end
+  end
+  
+end
