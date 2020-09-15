71e4aff5189302009f04e3786c455a51bcab1157
Avijit Karmaker <ajkacca@gmail.com>
2020-09-15 23:07:00 +0300

    methods are fixed based on given review

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/ModuleLength
 # rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 module Enumerable
   #----------my_each method-----------
@@ -30,37 +29,37 @@ module Enumerable
   #----------#my_all-----------
   def my_all?(arg = nil)
     if block_given?
-      my_each { |item| return false if yield(item) == false }
+      my_each { |item| return false unless yield(item) }
       return true
-    elsif arg.nil?
-      my_each { |item| return false if item.nil? || item == false }
-    elsif !arg.nil? && (arg.is_a? Class)
-      my_each { |item| return false if item.class != item }
-    elsif !arg.nil? && arg.class == Regexp
-      my_each { |item| return false unless arg.match(item) }
-    else
-      my_each { |item| return false if item != arg }
     end
-    true
+    if arg
+      case arg.class.to_s
+      when 'Class' then my_all? { |item| item.is_a?(arg) }
+      when 'Regexp' then my_all? { |item| item =~ arg }
+      else my_all? { |item| item == arg }
+      end
+    else
+      my_all? { |item| item }
+    end
   end
+
   #----------#my_any-----------
 
-  def my_any?(argument = nil)
+  def my_any?(arg = nil)
     if block_given?
       my_each { |item| return true if yield(item) }
-      false
-    elsif argument.nil?
-      my_each { |item| return true if item }
-    elsif !argument.nil? && (argument.is_a? Class)
-      my_each { |item| return true if item.class == argument }
-    elsif !argument.nil? && argument.class == Regexp
-      my_each { |item| return true if argument.match(item) }
-    else
-      my_each { |item| return true if item == argument }
+      return false
     end
-    false
+    if arg
+      case arg.class.to_s
+      when 'Class' then my_any? { |item| item.is_a?(arg) }
+      when 'Regexp' then my_any? { |item| item =~ arg }
+      else my_any? { |item| item == arg }
+      end
+    else
+      my_any? { |item| item }
+    end
   end
-
   #----------#my_select-----------
 
   def my_select
@@ -104,50 +103,27 @@ module Enumerable
   end
 
   #----------#my_inject-----------
-
   def my_inject(num = nil, sym = nil)
     if block_given?
       accumulator = num
-      my_each do |item|
-        accumulator = accumulator.nil? ? item : yield(accumulator, item)
-      end
+      my_each { |item| accumulator = accumulator.nil? ? item : yield(accumulator, item) }
       accumulator
     elsif !num.nil? && (num.is_a?(Symbol) || num.is_a?(String))
       accumulator = nil
-      my_each do |item|
-        accumulator = accumulator.nil? ? item : accumulator.send(num, item)
-      end
+      my_each { |item| accumulator = accumulator.nil? ? item : accumulator.send(num, item) }
       accumulator
     elsif !sym.nil? && (sym.is_a?(Symbol) || sym.is_a?(String))
       accumulator = num
-      my_each do |item|
-        accumulator = accumulator.nil? ? item : accumulator.send(sym, item)
-      end
+      my_each { |item| accumulator = accumulator.nil? ? item : accumulator.send(sym, item) }
       accumulator
+    else
+      raise LocalJumpError unless block_given?
     end
   end
 end
-# rubocop:enable Metrics/ModuleLength
+
 # rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 
 def multiply_els(arr)
   arr.my_inject { |result, element| result * element }
 end
-
-# start of testing
-
-# myarray = [1, 6, 3, 4, 5, 6, 12]
-
-# puts myarray.my_each {|item| puts item*2}
-# puts myarray.my_each_with_index {|item,index| puts"#{index} is #{item} "}
-# puts [1,21,1,3,45,13,5,12,nil].my_all?
-# puts [1,21,1,3,45,13,5,12,nil].my_all?{|item| item<50}
-# puts [1,2,3,nil].my_any?
-# puts myarray.my_select{|item| item<10}
-# puts (1...5).my_count(3)
-# puts myarray.my_none?
-# puts myarray.my_map {|item| item*5}
-# myproc=Proc.new{|item| item+20}
-# puts myarray.my_map(&myproc)
-# puts (1..10).my_inject(5){|result, element| result * element}
-# puts multiply_els(myarray)
