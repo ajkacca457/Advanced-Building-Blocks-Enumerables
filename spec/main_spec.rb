664e285d0cbd2eb621a21a2e189423f95b26abc6
Meron Ogbai <okbaymeron@gmail.com>
2020-10-01 17:27:58 +0300

    Add tests for my_each and my_select

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -24,4 +24,41 @@ RSpec.describe Enumerable do
     end
   end
 
+  # my_each_with_index method
+  describe 'my_each_with_index' do
+    it 'when an array is given it loop through an array and return each item with index ' do
+      expect(array.my_each_with_index { |item, index| puts "#{item} is #{index}" }). to equal(array.each_with_index { |item, index| puts "#{item} is #{index}" })
+    end
+
+    it 'when an range is given it loop through the range and return each item with index ' do
+      expect(range.my_each_with_index { |item, index| puts "#{item} is #{index}" }). to equal(range.each_with_index { |item, index| puts "#{item} is #{index}" })
+    end
+
+    it 'when a hash is given it loop through the hash and return each item with index ' do
+      expect(hash.my_each_with_index { |item, index| puts "#{item} is #{index}" }). to equal(hash.each_with_index { |item, index| puts "#{item} is #{index}" })
+    end
+    it 'when a block is not given it returns an enumerator' do
+      expect(array.my_each_with_index). to be_a(Enumerator)
+    end
+  end
+
+  # my_select
+
+  describe '#my_select' do
+    it 'when a array is given it loops through the array and return the item that match given condition' do
+      expect(array.my_select(&:odd?)).to eq([1, 3, 5, 7])
+    end
+
+    it 'when a range is given it loops through the range and return the item that match given condition' do
+      expect(range.my_select(&:odd?)).to eq([1, 3, 5, 7, 9])
+    end
+
+    it 'when a hash is given it loops through the hash and return the item that match given condition' do
+      expect(hash.my_select { |_item, value| value > 1200 }).to eq([[:mar, 1500], [:apr, 1300]])
+    end
+
+    it 'when a block is not given it returns an enumerator.' do
+      expect(array.my_select). to be_a(Enumerator)
+    end
+  end
 end
